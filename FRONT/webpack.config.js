1397e62d160e72f84dcbaca8f7a866b3b4561099
Martin Kang <martin.kang@efrei.net>
2024-09-23 04:21:34 +0200

    Add webpack.config.js and test production mode

--- FRONT/webpack.config.js
+++ FRONT/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development', 
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
